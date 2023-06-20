2630fcd905e6384571322060ba9c6682cfca0ccd
MssXCode <127544869+Chelvart@users.noreply.github.com>
2023-06-20 18:03:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
-var box = document.getElementById('box');
-var score = document.getElementById('score');
-var timer = document.getElementById('timer');
-var startButton = document.getElementById('start');
+document.addEventListener("DOMContentLoaded", function() {
+  const stickman = document.getElementById("stickman");
+  let isJumping = false;
 
-var currentScore = 0;
-var countdown = 10;
-var gameStarted = false;
-
-box.addEventListener('click', function() {
-  if (gameStarted) {
-    currentScore++;
-    score.textContent = 'Score: ' + currentScore;
+  function jump() {
+    if (!isJumping) {
+      isJumping = true;
+      stickman.style.bottom = "200px";
+      setTimeout(function() {
+        stickman.style.bottom = "0";
+        isJumping = false;
+      }, 500);
+    }
   }
-});
 
-startButton.addEventListener('click', function() {
-  if (!gameStarted) {
-    gameStarted = true;
-    startButton.disabled = true;
-    countdown = 10;
-    currentScore = 0;
-    score.textContent = 'Score: 0';
-    timer.textContent = 'Time: ' + countdown;
-
-    var interval = setInterval(function() {
-      countdown--;
-      timer.textContent = 'Time: ' + countdown;
-
-      if (countdown <= 0) {
-        clearInterval(interval);
-        gameStarted = false;
-        startButton.disabled = false;
-        alert('Game over! Your score is ' + currentScore);
-      }
-    }, 1000);
-  }
+  document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 32) {
+      jump();
+    }
+  });
 });
